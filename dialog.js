0121cb0cd018a7c3f506dc6e850df5efd04f38f7
LucasDevGames <lucasgamer213.com@gmail.com>
2017-12-15 04:22:12 +1300

    Update dialog.js

--- dialog.js
+++ dialog.js
@@ -1,56 +1,35 @@
-function CustomAlert(){
-	this.render = function(dialog){
-		var winW = window.innerWidth;
-	    var winH = window.innerHeight;
-		var dialogoverlay = document.getElementById('dialogoverlay');
-	    var dialogbox = document.getElementById('dialogbox');
-		dialogoverlay.style.display = "block";
-	    dialogoverlay.style.height = winH+"px";
-		dialogbox.style.left = (winW/2) - (550 * .5)+"px";
-	    dialogbox.style.top = "100px";
-	    dialogbox.style.display = "block";
-		document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
-	    document.getElementById('dialogboxbody').innerHTML = dialog;
-		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
-	}
-	this.ok = function(){
-		document.getElementById('dialogbox').style.display = "none";
-		document.getElementById('dialogoverlay').style.display = "none";
-	}
-}
-var Alert = new CustomAlert();
-function deletePost(id){
-	var db_id = id.replace("post_", "");
-	// Run Ajax request here to delete post from database
-	document.body.removeChild(document.getElementById(id));
-}
-function CustomConfirm(){
-	this.render = function(dialog,op,id){
-		var winW = window.innerWidth;
-	    var winH = window.innerHeight;
-		var dialogoverlay = document.getElementById('dialogoverlay');
-	    var dialogbox = document.getElementById('dialogbox');
-		dialogoverlay.style.display = "block";
-	    dialogoverlay.style.height = winH+"px";
-		dialogbox.style.left = (winW/2) - (550 * .5)+"px";
-	    dialogbox.style.top = "100px";
-	    dialogbox.style.display = "block";
-		
-		document.getElementById('dialogboxhead').innerHTML = "Confirm that action";
-	    document.getElementById('dialogboxbody').innerHTML = dialog;
-		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Confirm.yes(\''+op+'\',\''+id+'\')">Yes</button> <button onclick="Confirm.no()">No</button>';
-	}
-	this.no = function(){
-		document.getElementById('dialogbox').style.display = "none";
-		document.getElementById('dialogoverlay').style.display = "none";
-	}
-	this.yes = function(op,id){
-		if(op == "delete_post"){
-			deletePost(id);
-		}
-		document.getElementById('dialogbox').style.display = "none";
-		document.getElementById('dialogoverlay').style.display = "none";
-	}
-}
-var Confirm = new CustomConfirm();
 
+			function gotoMyChannel () {
+				if(confirm('Do you want to visit the channel?')){
+					window.location = 'https://youtube.com/c/SamSolomonPrabuSD';
+				}
+			}
+
+			function gotoMyChannelCustom () {
+				window.location = 'https://youtube.com/c/SamSolomonPrabuSD';
+			}
+
+			var CustomConfirm = new function(){
+
+				this.show = function (msg, callback) {
+					var dlg = document.getElementById('dialogCont');
+					var dlgBody = dlg.querySelector('#dlgBody');
+					dlg.style.top = '30%';
+					dlg.style.opacity = 1;
+					dlgBody.textContent = msg;
+					this.callback = callback;
+					document.getElementById('freezeLayer').style.display = '';
+				};
+
+				this.okay = function () {
+					this.callback();
+					this.close();
+				};
+
+				this.close = function () {
+					var dlg = document.getElementById('dialogCont');
+					dlg.style.top = '-30%';
+					dlg.style.opacity = 0;
+					document.getElementById('freezeLayer').style.display = 'none';
+				}
+			}
